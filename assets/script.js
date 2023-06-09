3d302da049cce467c674601397ff5f27f04d9cc7
Kevin M <crisstian2345@gmail.com>
2023-06-08 23:43:11 -0500

    add: optgroup

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,39 @@
 const synth = window.speechSynthesis;
 
-const modelSelect = document.getElementById('model');
 const createBtn = document.getElementById('createBtn');
-
-let voices = [];
-
+const voiceSelect = document.querySelector("select");
+let voices;
 
 function populateVoiceList() {
   voices = synth.getVoices();
+  let optgroups = {};
 
-  let _lang;
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name}`;
+    option.value = i;
 
-  for(const voice of voices){
-    const option = document.createElement('option');
-    option.textContent = `${voice.name} (${voice.lang})`;
-
-    if (voice.default) {
-      option.textContent += " — DEFAULT";
+    const lang = voices[i].lang;
+    if (!optgroups[lang]) {
+      optgroups[lang] = document.createElement("optgroup");
+      optgroups[lang].label = lang;
     }
+    optgroups[lang].appendChild(option);
+  }
 
-    option.setAttribute("data-lang", voice.lang);
-    option.setAttribute("data-name", voice.name);
-    modelSelect.appendChild(option);
+  for (const lang in optgroups) {
+    voiceSelect.appendChild(optgroups[lang]);
   }
 }
 
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+populateVoiceList();
+
 createBtn.addEventListener('click', () => {
-  populateVoiceList();
+  // populateVoiceList();
 })
 
 // populateVoiceList();
