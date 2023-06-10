fff54ed48b5596ab8dc422c2347d98e1ac021027
Kevin M <crisstian2345@gmail.com>
2023-06-10 02:46:27 -0500

    add: new style

--- assets/script.js
+++ assets/script.js
@@ -50,5 +50,35 @@ createBtn.addEventListener('click', () => {
     chrome.tabs.sendMessage(activeTab.id, { data: objt });
   });
 
-  // window.close();
+  window.close();
 })
+
+
+//local storage
+chrome.storage.sync.get(['model', 'rate', 'pitch'], function(result) {
+  // Restaura los valores seleccionados en los elementos select e input
+  if(result.model){
+    voiceSelect.value = result.model;
+  }
+  if (result.rate) {
+    rateElmnt.value = result.rate;
+  }
+  if (result.pitch) {
+    pitchElmnt.value = result.pitch;
+  }
+});
+
+// Maneja los cambios en los elementos select e input y guarda los valores
+voiceSelect.addEventListener('change', function() {
+  const selectedModel = voiceSelect.value;
+  chrome.storage.sync.set({ 'model': selectedModel });
+});
+rateElmnt.addEventListener('input', function() {
+  const selectedRate = rateElmnt.value;
+  chrome.storage.sync.set({ 'rate': selectedRate });
+});
+
+pitchElmnt.addEventListener('input', function() {
+  const selectedPitch = pitchElmnt.value;
+  chrome.storage.sync.set({ 'pitch': selectedPitch });
+});
